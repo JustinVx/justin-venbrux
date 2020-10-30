a679fdb44dd0ff29fe440a4e13c82922a0c84530
“JustinVx” <justin.venbrux@gmail.com>
2020-10-30 19:05:44 +0100

    Some testing

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,18 @@ export default function About() {
       <p>
         How to create a needy robot? This is a question you may have never asked yourself, but I have! Together with a group of five others I built three prototypes and we improved the interaction on every iteration. I worked as one of the two engineers of the group, where I worked on building an programming the actual robots. I also worked a lot on perfecting the interaction. This project involved an Arduino, laser cutting, sewing, electronics, a lot of testing and cuteness.
       </p>
+      <p>
+        How to create a needy robot? This is a question you may have never asked yourself, but I have! Together with a group of five others I built three prototypes and we improved the interaction on every iteration. I worked as one of the two engineers of the group, where I worked on building an programming the actual robots. I also worked a lot on perfecting the interaction. This project involved an Arduino, laser cutting, sewing, electronics, a lot of testing and cuteness.
+      </p>
+      <p>
+        How to create a needy robot? This is a question you may have never asked yourself, but I have! Together with a group of five others I built three prototypes and we improved the interaction on every iteration. I worked as one of the two engineers of the group, where I worked on building an programming the actual robots. I also worked a lot on perfecting the interaction. This project involved an Arduino, laser cutting, sewing, electronics, a lot of testing and cuteness.
+      </p>
+      <p>
+        How to create a needy robot? This is a question you may have never asked yourself, but I have! Together with a group of five others I built three prototypes and we improved the interaction on every iteration. I worked as one of the two engineers of the group, where I worked on building an programming the actual robots. I also worked a lot on perfecting the interaction. This project involved an Arduino, laser cutting, sewing, electronics, a lot of testing and cuteness.
+      </p>
+      <p>
+        How to create a needy robot? This is a question you may have never asked yourself, but I have! Together with a group of five others I built three prototypes and we improved the interaction on every iteration. I worked as one of the two engineers of the group, where I worked on building an programming the actual robots. I also worked a lot on perfecting the interaction. This project involved an Arduino, laser cutting, sewing, electronics, a lot of testing and cuteness.
+      </p>
     </Layout>
   )
 }
