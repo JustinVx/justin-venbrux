4e3a5d212991494dd6062319b55b234c34e33271
“JustinVx” <justin.venbrux@gmail.com>
2020-10-30 19:05:26 +0100

    Added filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
     title: `Justin Venbrux`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
