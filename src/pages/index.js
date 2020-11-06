9c6cf7eaf62c310089333c1e2b5e5a7969309618
“JustinVx” <justin.venbrux@gmail.com>
2020-11-06 18:34:12 +0100

    Changed UI home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,23 @@ import Layout from "../components/layout"
 import { rhythm } from "../utils/typography"
 
 const Hero = styled.div`
-  font-family: "Alegreya Sans", "Open Sans", serif;
-  text-align: left;
-  max-width: 300px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  .hero-text {
+    font-family: "Alegreya Sans", "Open Sans", serif;
+    text-align: right;
+    max-width: 300px;
+  }
+
+  .hero-image {
+    background-color: white;
+    width: 250px;
+    height: 250px;
+    justify-content: center;
+    box-shadow: 0px 0px 12px #ddd;
+    transform: rotate(2deg);
+  }
 
   h1 {
     font-size: 2.4em;
@@ -21,28 +35,38 @@ const Hero = styled.div`
   }
 `
 
+const Work = styled.div`
+  background-color: #f5f5f5;
+  margin-left: 0;
+`
+
 export default function Index({ data }) {
   return (
     <Layout>
       <Hero>
-        <h1>
-          I'm <b>Justin</b>.
-        </h1>
-        <h3>
-          I design cool things that help people. 🚀
-        </h3>
+        <div class="hero-text">
+          <h1>
+            I'm <b>Justin</b>.
+          </h1>
+          <h3>
+            I design cool things that help people. 🚀
+          </h3>
+        </div>
+        <div class="hero-image">
+        </div>
       </Hero>
+      <Work>
+        <h2>Work</h2>
+        <p>
+          We're the only site running on your computer dedicated to showing the
+          best photos and videos of pandas eating lots of food.
+        </p>
+        <p>
+          Here's <Link to={`/blog/`} >Blog</Link> and a <Link to={`/about/`} >another page</Link>.
+        </p>
+      </Work>
       <br />
       <br />
-      <h2>Work</h2>
-      <p>
-        We're the only site running on your computer dedicated to showing the
-        best photos and videos of pandas eating lots of food.
-      </p>
-      <p>
-        Here's <Link to={`/blog/`} >Blog</Link> and a <Link to={`/about/`} >another page</Link>.
-      </p>
-      
     </Layout>
   )
 }
