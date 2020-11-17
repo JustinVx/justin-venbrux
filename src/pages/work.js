9674c8c94d385786956888a40d8cecb1df0c09a7
“JustinVx” <justin.venbrux@gmail.com>
2020-11-17 11:14:44 +0100

    Updated intro for work and writing pages

--- src/pages/work.js
+++ src/pages/work.js
@@ -10,6 +10,21 @@ import tamingLogo from "../images/taming-logo.png"
 import genzo from "../images/genzo-home.jpg"
 import justin from "../images/black-white.jpg"
 
+const AboutPageContainer = styled.div`
+  margin: 0 auto;
+  max-width: 730px;
+  padding: ${rhythm(2)};
+  padding-top: ${rhythm(1.5)};
+  padding-bottom: 0;
+`
+
+const AboutPage = styled.div`
+  font-size: 1.4rem;
+  font-weight: normal;
+  margin-top: ${rhythm(1)};
+  margin-bottom: ${rhythm(2)};
+`
+
 const WorkContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -29,6 +44,11 @@ export default function Index() {
   return (
       <>
       <Header />
+      <AboutPageContainer>
+        <AboutPage>
+            Below is a selection of projects I worked on. You can always <a href="mailto:info@justinvenbrux.nl">e-mail me</a> for questions, comments or suggestions. 🥽 
+        </AboutPage>
+      </AboutPageContainer>
       <WorkContainer>
         <Work>
           <ProjectCard 
