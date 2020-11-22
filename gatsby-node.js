3334bc06a53d4cc9145d356ebdd69ec2aff73f4d
“JustinVx” <justin.venbrux@gmail.com>
2020-11-22 15:47:28 +0100

    conditional template based on work or writing content

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,6 +23,9 @@ exports.createPages = async ({ graphql, actions }) => {
             fields {
               slug
             }
+            frontmatter {
+              type
+            }
           }
         }
       }
@@ -30,9 +33,12 @@ exports.createPages = async ({ graphql, actions }) => {
   `)
 
   result.data.allMdx.edges.forEach(({ node }) => {
+    const defaultTemplate = path.resolve(`./src/templates/blog-post.js`)
+    const projectTemplate = path.resolve(`./src/templates/project.js`)
+    const correctTemplate = (node.frontmatter.type === "work") ? projectTemplate : defaultTemplate
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
+      component: correctTemplate,
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
