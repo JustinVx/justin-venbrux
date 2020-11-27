52c3c846785d088b277edb9160c2ddc159115022
“JustinVx” <justin.venbrux@gmail.com>
2020-11-27 12:28:19 +0100

    Created Title and Subtitle components

---
+++ src/components/subTitle.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+import { rhythm } from "../utils/typography"
+
+const SubTitle = styled.span`
+  text-transform: uppercase;
+  font-size: 0.7rem;
+  color: #888;
+
+  line-height: 0px;
+`
+
+export default SubTitle;
---
+++ src/components/title.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled"
+import { rhythm } from "../utils/typography"
+
+const Title = styled.h1`
+  font-size: 3.3rem;
+  margin-bottom: ${rhythm(0.3)};
+`
+
+export default Title;
