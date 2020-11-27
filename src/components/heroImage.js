07892fbf9f6a3cb056000222bc5ced5b34bedf52
“JustinVx” <justin.venbrux@gmail.com>
2020-11-27 12:27:28 +0100

    Added margin to HeroImage

--- src/components/heroImage.js
+++ src/components/heroImage.js
@@ -1,13 +1,12 @@
 import React from "react"
 import styled from "@emotion/styled"
+import { rhythm } from "../utils/typography"
 
 const Image = styled.div`
   width: 830px;
   box-shadow: 0 30px 60px -10px rgba(0,0,0,0.2),0 18px 36px -18px rgba(0,0,0,0.22);
-
-  figcaption {
-    transform: scale(0);
-  }
+  margin-top: ${rhythm(2)};
+  margin-bottom: ${rhythm(2)};
 `
 
 export default function HeroImage({ children }) {
