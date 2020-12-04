f8fdb8106efbd6ea42af86c742bd069b1646adee
“JustinVx” <justin.venbrux@gmail.com>
2020-12-04 17:10:52 +0100

    HeroImage edited for mobile devices

--- src/components/heroImage.js
+++ src/components/heroImage.js
@@ -7,6 +7,18 @@ const Image = styled.div`
   box-shadow: 0 30px 60px -10px rgba(0,0,0,0.2),0 18px 36px -18px rgba(0,0,0,0.22);
   margin-top: ${rhythm(2)};
   margin-bottom: ${rhythm(2)};
+
+  @media(max-width:900px) {
+    width: 750px;
+  }
+
+  @media(max-width:768px) {
+    width: 590px;
+  }
+
+  @media(max-width:595px) {
+    width: 90%;
+  }
 `
 
 export default function HeroImage({ children }) {
