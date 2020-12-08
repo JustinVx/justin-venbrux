6119462c956cff7962fe9ed2658b88596eacff77
“JustinVx” <justin.venbrux@gmail.com>
2020-12-08 16:24:54 +0100

    Create EmbedVideo component

---
+++ src/components/embedVideo.js
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled"
+
+const EmbedVideo = styled.div`
+  position: relative;
+  display: block;
+  width: 100%;
+  padding: 0 ;
+  overflow: hidden;
+  padding-top: 15px;
+
+  video {
+    width: 100%;
+  }
+`
+
+export default EmbedVideo;
