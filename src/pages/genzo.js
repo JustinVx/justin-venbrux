d90ef634f16c174016808b221275b78deed628ae
“JustinVx” <justin.venbrux@gmail.com>
2020-12-15 14:03:04 +0100

    Added linked genzo page to venbrux.net

---
+++ src/pages/genzo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Genzo extends Component {
+  componentDidMount() {
+      window.location.replace("https://www.venbrux.net/genzo/");
+  }
+
+  render() {
+      return <div />
+  }
+}
+
+export default Genzo
